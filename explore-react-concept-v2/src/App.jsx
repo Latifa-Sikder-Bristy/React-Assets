f0e0f9addebbe9a09bc660a54cf7bf02a65afe67
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-09-23 10:54:04 +0600

    first commit

---
+++ explore-react-concept-v2/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // 1. using normal function
+  function handleClicked() {
+    alert('button clicked')
+  }
+  const adToFive = (number) => {
+    alert(number + 5);
+  }
+
+  // 2. using arrow function
+  const handleClicked2 = () => {
+    alert('button clicked')
+  }
+  // 2. using inline arrow function : line 20
+  // if I declear adToFive(3) function direct onclick button it will direact call to the function. Rather than to onclick apply inside a arrow function Or wrap with arrow function
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button onClick={handleClicked}>Click Me</button>
+      <button onClick={handleClicked2}>Click Me 2</button>
+      <button onClick={() => { alert('Third clicked') }}>Click Me 3</button>
+      <button onClick={() => adToFive(3)}>Click Me 4</button>
+    </>
+  )
+}
+
+export default App
