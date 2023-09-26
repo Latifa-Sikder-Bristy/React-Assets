2215f59e703d9bb00b1a52007926e3356b021e49
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-09-26 15:06:28 +0600

    Conditional CSS and conditional style

--- react-world-tour/src/components/Countries/Countries.jsx
+++ react-world-tour/src/components/Countries/Countries.jsx
@@ -4,20 +4,34 @@ import './countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitCountries, setVisitedCountries] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => setCountries(data));
     }, [])
+
+    const handleVisitedCountry = (country) => {
+        console.log('add this to visited country');
+        console.log(country);
+    }
     return (
         <div>
             <h3>Countries {countries.length}</h3>
+            <div>
+                <h5>Visited Countires</h5>
+                <ul>
+
+                </ul>
+            </div>
             <div className="country-container">
                 {
                     countries.map(country => <Country
                         key={country.ccn3}
-                        country={country}></Country>)
+                        country={country}
+                        handleVisitedCountry={handleVisitedCountry}
+                    ></Country>)
                 }
             </div>
         </div>
--- react-world-tour/src/components/Country/Country.jsx
+++ react-world-tour/src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     const { name, flags, population, area, cca3 } = country || {};
 
     const [visited, setVisited] = useState(false);
@@ -9,15 +9,17 @@ const Country = ({ country }) => {
     const handleVisited = () => {
         setVisited(!visited);
     }
-    console.log(JSON.stringify(population))
+    console.log(handleVisitedCountry);
     return (
-        <div className='country'>
-            <h3>{name?.common}</h3>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: visited ? 'purple' : 'white' }}>{name?.common}</h3>
             <img className='flag-height' src={flags?.png} alt="" />
             <p>Population: {population}</p>
             {/* <p>Population: {population?.toString().split("").join(",")}</p> */}
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button>Mark visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? '  I have visited this country' : '  I want to visit'}
         </div>
