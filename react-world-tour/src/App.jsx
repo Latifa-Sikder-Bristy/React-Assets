43a74d9b6ba85776dcc94c42b8083ec4443b276d
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-10-09 12:44:34 +0600

    version 1.1

--- react-world-tour/src/App.jsx
+++ react-world-tour/src/App.jsx
@@ -2,16 +2,13 @@ import './App.css'
 import Countries from './components/Countries/Countries'
 import Country from './components/Country/Country'
 
-
 function App() {
   return (
     <>
       <h1>React Awesome World Tour!!!</h1>
       <Countries></Countries>
       <Country></Country>
-
     </>
   )
 }
-
 export default App
